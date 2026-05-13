910f90a6c8109f107cd577691ab767056cc93fc9
PalakVaghela <palakvaghela83@gmail.com>
2026-05-13 15:16:59 +0530

    [IMP] guardx: Code formate

--- dashboard/guardx_dashboard/src/App.jsx
+++ dashboard/guardx_dashboard/src/App.jsx
@@ -1,52 +1,47 @@
-
-
 import { useEffect, useState } from 'react'
 import './App.css'
-import { getOverview , getRPS, getIps} from './services/api'
-import {LineChart, XAxis, Line, YAxis, Tooltip, CartesianGrid} from 'recharts';
+import { getOverview, getRPS, getIps } from './services/api'
+import { LineChart, XAxis, Line, YAxis, Tooltip, CartesianGrid } from 'recharts'
 
-function App() {
-
-  const [matrics, setMetrics] = useState({total:0, blocked:0});
-  const [rps, setRps] = useState([]);
-  const [topIps, setTopIps] = useState([]);
+function App () {
+  const [matrics, setMetrics] = useState({ total: 0, blocked: 0 })
+  const [rps, setRps] = useState([])
+  const [topIps, setTopIps] = useState([])
 
   useEffect(() => {
-
     // By putting it inside, you are telling React: "This function is only for this specific effect."
-    async function fetchData() {
-      const data = await getOverview();
-      setMetrics(data);
+    async function fetchData () {
+      const data = await getOverview()
+      setMetrics(data)
 
-      const rps = await getRPS();
-      setRps(rps);
+      const rps = await getRPS()
+      setRps(rps)
 
-      const ips = await getIps();
-      const formatted = [];
+      const ips = await getIps()
+      const formatted = []
       // ips will get like this below type, firest ip then req in list form, so will will take each 0th ip, while 1st is reqs., so that we just through +2
       // [
       //   "::ffff:127.0.0.1",
       //   "15"
       // ]
-      for(let i = 0; i < ips.length; i += 2) {
+      for (let i = 0; i < ips.length; i += 2) {
         formatted.push({
           ip: ips[i],
           requests: ips[i + 1]
-        });
-      };
+        })
+      }
 
-      setTopIps(formatted);
-
+      setTopIps(formatted)
     }
 
-    fetchData();
+    fetchData()
 
     // This will call the fun in every 2sec and make the dashboard live.
-    const interval = setInterval(fetchData, 2000);
-    return() => clearInterval(interval)
-  }, []);
+    const interval = setInterval(fetchData, 2000)
+    return () => clearInterval(interval)
+  }, [])
 
-  return(
+  return (
     <div>
       <h1>Hello this is dashboard</h1>
       <div>
@@ -61,16 +56,15 @@ function App() {
 
       <h2>Live request per second:</h2>
       <LineChart width={700} height={300} data={rps}>
-        <CartesianGrid strokeDasharray="3 3"/>
-        <XAxis dataKey="second"/>
-        <YAxis/>
-        <Tooltip/>
-        <Line type="monotone" dataKey="requests" stroke="#8884d8"/>
+        <CartesianGrid strokeDasharray='3 3' />
+        <XAxis dataKey='second' />
+        <YAxis />
+        <Tooltip />
+        <Line type='monotone' dataKey='requests' stroke='#8884d8' />
       </LineChart>
 
       <h2>Top IPs</h2>
-      <table border="1">
-
+      <table border='1'>
         <thead>
           <tr>
             <th>IP</th>
@@ -79,24 +73,17 @@ function App() {
         </thead>
 
         <tbody>
-
           {topIps.map((item, index) => (
-
             <tr key={index}>
-
               <td>{item.ip}</td>
 
               <td>{item.requests}</td>
-
             </tr>
-
           ))}
-
         </tbody>
-
       </table>
-          </div>
-        )
+    </div>
+  )
 }
 
 export default App
